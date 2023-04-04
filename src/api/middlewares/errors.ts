5042e96882f322262443acf748d681c0d391e524
Yeramin <118217455+ysfdev2@users.noreply.github.com>
2023-04-04 19:09:13 -0400

    Enable cors support (#40)
    Co-authored-by: ysfdev <13709931+ysfdev@users.noreply.github.com>

--- src/api/middlewares/errors.ts
+++ src/api/middlewares/errors.ts
@@ -2,14 +2,20 @@ import { Request, Response, NextFunction } from '../utils';
 
 export const errorHandler = (
   err: Error,
-  _: Request,
+  req: Request,
   res: Response,
   next: NextFunction
 ) => {
   if (res.headersSent) {
     return next(err)
   }
-  res.status(500).render('error', { error: err });
+
+  res.status(500);
+  if (req.get('Content-Type') === 'application/json') {
+    res.json({ error: err });
+  } else {
+    res.render('error', { error: err });
+  }
 };
 
 export default errorHandler
